a7f44be740d23cb2efb3ef88d54686ef4e489579
Tim Harvey <tharvey@gateworks.com>
2014-06-10 10:43:04 -0700

    mxc_ipuv3: show ipu_id/disp_id of registered device
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- drivers/video/mxc/mxc_ipuv3_fb.c
+++ drivers/video/mxc/mxc_ipuv3_fb.c
@@ -1934,7 +1934,8 @@ static int mxcfb_dispdrv_init(struct platform_device *pdev,
 	mxcfbi->dispdrv = mxc_dispdrv_gethandle(disp_dev, &setting);
 	if (IS_ERR(mxcfbi->dispdrv)) {
 		ret = PTR_ERR(mxcfbi->dispdrv);
-		dev_err(&pdev->dev, "NO mxc display driver found!\n");
+		dev_err(&pdev->dev, "NO mxc display driver found for %s!\n",
+			disp_dev);
 		return ret;
 	} else {
 		/* fix-up  */
@@ -1949,6 +1950,8 @@ static int mxcfb_dispdrv_init(struct platform_device *pdev,
 				setting.if_fmt, setting.default_bpp,
 				mxcfbi->ipu_di, mxcfbi->ipu_id);
 	}
+	dev_info(&pdev->dev, "registered mxc display driver %s IPU%d_DISP%d\n",
+		 disp_dev, mxcfbi->ipu_id + 1, mxcfbi->ipu_di);
 
 	dev_info(&pdev->dev, "registered mxc display driver %s\n", disp_dev);
 
