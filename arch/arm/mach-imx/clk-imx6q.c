3d0243f22a1e2a76609c61021f9197f62fc05825
ektor5 <ek5.chimenti@gmail.com>
2015-06-04 17:21:10 +0200

    Set PLL5_VIDEO as main parent of LDB_DI0
    This fix the issue on the UDOO LVDS 7", overriding any setting made in uboot

--- arch/arm/mach-imx/clk-imx6q.c
+++ arch/arm/mach-imx/clk-imx6q.c
@@ -811,6 +811,9 @@ static void __init imx6q_clocks_init(struct device_node *ccm_node)
 		imx_clk_set_parent(clk[IMX6QDL_CLK_IPU2_SEL], clk[IMX6QDL_CLK_MMDC_CH0_AXI]);
 	}
 
+	/* set pll5_video as default pll for lvds */
+	imx_clk_set_parent(clk[IMX6QDL_CLK_LDB_DI0_SEL], clk[IMX6QDL_CLK_PLL5_VIDEO_DIV]);
+	
 	/*
 	 * The gpmi needs 100MHz frequency in the EDO/Sync mode,
 	 * We can not get the 100MHz from the pll2_pfd0_352m.
