ca2d5c1f9944150b88d44ee7de1d10d3fb63ce5f
waynatali <waykovalenko@gmail.com>
2015-06-10 12:57:48 +0200

    Make soc_ac97_dev_register function non static

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -523,7 +523,7 @@ static int soc_ac97_dev_unregister(struct snd_soc_codec *codec)
 static void soc_ac97_device_release(struct device *dev){}
 
 /* register ac97 codec to bus */
-static int soc_ac97_dev_register(struct snd_soc_codec *codec)
+int soc_ac97_dev_register(struct snd_soc_codec *codec)
 {
 	int err;
 
