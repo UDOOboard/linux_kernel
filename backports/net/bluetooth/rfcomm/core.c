72ff46172f393528d26f628916fcd5a8f327278c
Robert Nelson <robertcnelson@gmail.com>
2015-09-17 15:45:16 -0500

    apply backport SmPL patch collateral-evolutions/network/0031-sk_data_ready.cocci

--- backports/net/bluetooth/rfcomm/core.c
+++ backports/net/bluetooth/rfcomm/core.c
@@ -193,6 +193,11 @@ static void rfcomm_l2data_ready(struct sock *sk)
 	BT_DBG("%p", sk);
 	rfcomm_schedule();
 }
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,15,0)
+static void backport_rfcomm_l2data_ready(struct sock *sk, int unused){
+	rfcomm_l2data_ready(sk);
+}
+#endif
 
 static int rfcomm_l2sock_create(struct socket **sock)
 {
@@ -203,7 +208,11 @@ static int rfcomm_l2sock_create(struct socket **sock)
 	err = sock_create_kern(PF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP, sock);
 	if (!err) {
 		struct sock *sk = (*sock)->sk;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,15,0)
 		sk->sk_data_ready   = rfcomm_l2data_ready;
+#else
+		sk->sk_data_ready = backport_rfcomm_l2data_ready;
+#endif
 		sk->sk_state_change = rfcomm_l2state_change;
 	}
 	return err;
@@ -1944,7 +1953,11 @@ static void rfcomm_accept_connection(struct rfcomm_session *s)
 		return;
 
 	/* Set our callbacks */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,15,0)
 	nsock->sk->sk_data_ready   = rfcomm_l2data_ready;
+#else
+	nsock->sk->sk_data_ready = backport_rfcomm_l2data_ready;
+#endif
 	nsock->sk->sk_state_change = rfcomm_l2state_change;
 
 	s = rfcomm_session_add(nsock, BT_OPEN);
